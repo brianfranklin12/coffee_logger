574c9426bb6a96cedbddffac250935faf382156d
Brian Franklin <brianfranklinmusic@gmail.com>
2021-04-28 12:40:37 -0400

    add api call in main to render inital table load

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,6 +2,7 @@
 const addCoffeeBtn = document.querySelector(".add-coffee-btn");
 const modal = document.querySelector(".bg-modal");
 const modalClose = document.querySelector(".close");
+const coffeeTable = document.querySelector(".coffee-table");
 
 // Event Listeners
 
@@ -12,3 +13,10 @@ addCoffeeBtn.addEventListener("click", () => {
 modalClose.addEventListener("click", () => {
   modal.style.display = "none";
 })
+
+ApiService.getCoffees()
+.then(data => data.forEach((coffeeData) => {
+  console.log(coffeeData);
+  let newCoffee = new Coffee(coffeeData);
+  newCoffee.renderTableRow()
+}));
