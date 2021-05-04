2662300ea280f0c8ffef36ae2aa826e109ddcef7
Brian Franklin <brianfranklinmusic@gmail.com>
2021-05-04 15:12:49 -0400

    render log table data to dom

--- frontend/js/log.js
+++ frontend/js/log.js
@@ -12,6 +12,37 @@ class Log {
   }
 
   renderGridRow() {
-    console.log(this)
+    const main = document.querySelector('main');
+    const container = document.createElement('div');
+    container.className = "container"
+    const logTable = document.createElement('div');
+    logTable.className = "log-table"
+    const row = document.createElement('div');
+    row.className = "log-grid-row";
+    const logDose = document.createElement('div');
+    logDose.className = "log-col-dose";
+    logDose.innerText = this.dose;
+    const logOutput = document.createElement('div');
+    logOutput.className = "log-col-output";
+    logOutput.innerText = this.output;
+    const logGrindSize = document.createElement('div');
+    logGrindSize.className = "log-col-grind-size";
+    logGrindSize.innerText = this.grind_size;
+    const logBrewTime = document.createElement('div');
+    logBrewTime.className = "log-col-brew-time";
+    logBrewTime.innerText = this.brew_time;
+    const logBrewMethod = document.createElement('div');
+    logBrewMethod.className = "log-col-brew-method";
+    logBrewMethod.innerText = this.brew_method;
+    const logRating = document.createElement('div');
+    logRating.className = "log-col-rating";
+    logRating.innerText = this.rating;
+    const logNotes = document.createElement('div');
+    logNotes.className = "log-col-notes";
+    logNotes.innerText = this.notes;
+    row.append(logDose, logOutput, logGrindSize, logBrewTime, logBrewMethod, logRating, logNotes);
+    logTable.append(row);
+    container.append(logTable);
+    main.append(container);
   }
 }
