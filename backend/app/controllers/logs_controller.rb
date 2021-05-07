105b860f78f97586530beb27375e3150aecf70ad
Brian Franklin <brianfranklinmusic@gmail.com>
2021-05-06 21:42:51 -0400

    add create and destroy method to logs controller

--- backend/app/controllers/logs_controller.rb
+++ backend/app/controllers/logs_controller.rb
@@ -1,6 +1,27 @@
 class LogsController < ApplicationController
   def index
-    logs = Log.all
+    logs = Coffee.find(params[:coffee_id]).logs
     render json: logs, include: [:coffee]
   end
+
+  def create
+    log = Log.new(log_params)
+    if log.save
+      render json: log, inlcude: [:coffee]
+    else
+      render json: { error: "Log not successfully created" }
+    end
+  end
+
+  def destroy
+    log = Log.find_by(id: params[:id])
+    log.delete
+  end
+
+  private
+  
+  def log_params
+    params.require(:log).permit(:dose, :output, :grind_size, :brew_time, :brew_method, :rating, :notes)
+  end
+
 end
